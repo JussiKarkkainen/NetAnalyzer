a38df9adc8add46bfb117feff0b600c5e8d3bfaa
Jussi Karkkainen <juskark@gmail.com>
2023-01-02 21:59:50 +0200

    transform ip addrs

--- inject_new.c
+++ inject_new.c
@@ -9,13 +9,26 @@
 #include <net/ethernet.h>
 #include <netinet/if_ether.h>
 
+struct ip_addrs {
+    uint32_t ip_one;
+    uint32_t ip_two;
+};
 
 int initialize_inject(char *gateway_ip, char *target_ip) {
     
     printf("Initializing inject\n");
 
-    // Given the two IP addresses, find out the MAC addresses
-    
+    // Convert IP address strings to numerical form
+    struct ip_addrs *ip_num;
+    ip_num->ip_one = inet_addr(gateway_ip);
+    ip_num->ip_two = inet_addr(target_ip);
+
+    if (ip_num->ip_one == -1 || ip_num->ip_two == -1) {
+        perror("inet_addr() returned an error: ");
+        exit(1);
+    }
+
+    // Given the two IP addresses, find out the MAC addresses   
 
 
     // Construct ARP Packets for spoofing
