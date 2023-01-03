75b965b0c4b116eab92a3c04d3a4649fa45b404a
Jussi Karkkainen <juskark@gmail.com>
2023-01-03 13:49:15 +0200

    get MAC addr

--- inject_new.c
+++ inject_new.c
@@ -8,13 +8,14 @@
 #include <netpacket/packet.h>
 #include <net/ethernet.h>
 #include <netinet/if_ether.h>
+#include "headers.h"
 
 struct ip_addrs {
     uint32_t ip_one;
     uint32_t ip_two;
 };
 
-int initialize_inject(char *gateway_ip, char *target_ip) {
+int initialize_inject(char *gateway_ip, char *target_ip, uint8_t *own_mac) {
     
     printf("Initializing inject\n");
 
@@ -29,10 +30,12 @@ int initialize_inject(char *gateway_ip, char *target_ip) {
     }
 
     // Given the two IP addresses, find out the MAC addresses   
+    uint8_t *mac_addr = get_mac_addr(ip_num->ip_one, own_mac);
+    uint8_t *mac_addr = get_mac_addr(ip_num->ip_one, own_mac);
 
 
     // Construct ARP Packets for spoofing
-
+    
 
 
     // Start the spoofing and analyze packets    
@@ -46,5 +49,65 @@ int initialize_inject(char *gateway_ip, char *target_ip) {
     }    
     
     return 0;
+}
+
+
+uint8_t *get_mac_addr(uint32_t ip_addr, uint8_t *own_mac) {
+    
+    struct arp_header *arp_hdr;
+
+    arp_hdr->hardware_type = htons(1);
+    arp_hdr->protocol_type = htons(0x0800);
+    arp_hdr->hardware_size = 6;
+    arp_hdr->protocol_size = 4;
+    arp_hdr->opcode = htons(1);         // ARP request = 1, ARP_REPLY = 2
+    
+    memcpy(arp_hdr->sha, own_mac, 6);
+    memcpy(arp_hdr->spa, own_ip, 4);
+    memcpy(arp_hdr->tha, 0, 6);
+    memcpy(arp_hdr->tpa, ip_addr, 4);
+
+    struct sockaddr_ll dest_addr = {0};
+    dest_addr.sll_family = AF_PACKET;
+    dest_addr.sll_protocol = htons(ETH_P_ARP);
+    dest_addr.sll_ifindex = if_nametoindex(ifname);
+    dest_addr.sll_halen = 6;
+
+    uint8_t broadcast_mac[MAC_LEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+    memcpy(dest_addr.sll_addr, broadcast_mac, 6);
+    
+    int sock = socket(AF_INET, SOCK_RAW, htons(ETH_P_ALL));
+    
+    if (sock == -1) {
+        perror("Error: socket()");
+        return 1;
+    }
+
+    
+    if (bind(sockfd, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+        perror("bind failed");
+        return -1;
+    }
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("setsockopt failed");
+        return -1;
+    }
+
+    int bytes_sent = sendto(sockfd, req, sizeof(struct arp_header), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (bytes_sent < 0) {
+        perror("sendto failed");
+        return -1;
+    }
+    
+    int bytes_recvd = recvfrom(sockfd, res, sizeof(struct arp_header), 0, NULL, NULL);
+    if (bytes_recvd < 0) {
+        return -1;
+    }
+    return bytes_recvd;
 }
 
--- inject_new.h
+++ inject_new.h
@@ -1,6 +1,7 @@
 #ifndef INJECT_NEW_H
 #define INJECT_NEW_H
 
+#define ARP_REQUEST 1
 
 
 #endif
